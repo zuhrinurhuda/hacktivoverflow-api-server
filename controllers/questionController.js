652c327cee84f8299d1b46a56b06ff9a63ff9532
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2018-01-16 00:19:29 +0700

    add feature upvote & downvote question

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -6,7 +6,7 @@ class QuestionController {
       title: req.body.title,
       content: req.body.content,
       author: req.decoded._id,
-      slug: req.body.slug
+      slugs: req.body.slugs
     })
 
     newQuestion.save()
@@ -39,12 +39,62 @@ class QuestionController {
     .catch(err => res.status(500).send(err))
   }
 
+  static upVote (req, res) {
+    Question.findById(req.params.id)
+    .then(question => {
+      let userUpVoteIndex = question.upVoters.findIndex(element => element == req.decoded._id)
+      let userDownVoteIndex = question.downVoters.findIndex(element => element == req.decoded._id)
+
+      if (userUpVoteIndex === -1 && userDownVoteIndex === -1) { // false && false
+        question.upVoters.push(req.decoded._id)
+      } else if (userUpVoteIndex === -1 && userDownVoteIndex !== -1) { // false && true
+        question.downVoters.splice(userDownVoteIndex, 1)
+        question.upVoters.push(req.decoded._id)
+      } else if (userUpVoteIndex !== -1 && userDownVoteIndex === -1) { // true && false
+        question.upVoters.splice(userUpVoteIndex, 1)
+      }
+
+      question.save()
+      .then(newQuestionData => res.status(200).json({
+        message: 'Success vote question',
+        updatedQuestion: newQuestionData
+      }))
+      .catch(err => res.status(500).send(err))
+    })
+    .catch(err => res.status(500).send(err))
+  }
+
+  static downVote(req, res) {
+    Question.findById(req.params.id)
+    .then(question => {
+      let userUpVoteIndex = question.upVoters.findIndex(element => element == req.decoded._id)
+      let userDownVoteIndex = question.downVoters.findIndex(element => element == req.decoded._id)
+
+      if (userDownVoteIndex === -1 && userUpVoteIndex === -1) { // false && false
+        question.downVoters.push(req.decoded._id)
+      } else if (userDownVoteIndex === -1 && userUpVoteIndex !== -1) { // false && true
+        question.upVoters.splice(userUpVoteIndex, 1)
+        question.downVoters.push(req.decoded._id)
+      } else if (userDownVoteIndex !== -1 && userUpVoteIndex === -1) { // true && false
+        question.downVoters.splice(userDownVoteIndex, 1)
+      }
+
+      question.save()
+      .then(newQuestionData => res.status(200).json({
+        message: 'Success vote question',
+        updatedQuestion: newQuestionData
+      }))
+      .catch(err => res.status(500).send(err))
+    })
+    .catch(err => res.status(500).send(err))
+  }
+
   static update (req, res) {
     Question.findById(req.params.id)
     .then(question => {
       question.title = req.body.title || question.title
       question.content = req.body.content || question.content
-      question.slug = req.body.slug || question.slug
+      question.slugs = req.body.slugs || question.slugs
       question.save()
       .then(newQuestionData => res.status(200).json({
         message: 'Success update data question',
--- routes/questions.js
+++ routes/questions.js
@@ -5,6 +5,8 @@ const checkAuth = require('../middleware/checkAuth')
 router.post('/', checkAuth.isLogin, question.create)
 router.get('/', question.findAll)
 router.get('/:id', question.findById)
+router.put('/:id/upvote', checkAuth.isLogin, question.upVote)
+router.put('/:id/downvote', checkAuth.isLogin, question.downVote)
 router.put('/:id', checkAuth.isLogin, question.update)
 router.delete('/:id', checkAuth.isLogin, question.delete)
 
